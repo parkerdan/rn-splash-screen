b27e50f0a29f50c7548f4ab0c912c4091f68f04a
parkerdan <parker.daniel.o@gmail.com>
2018-03-12 13:37:59 -0700

    fix errors

--- ios/RNSplashScreen/RNSplashScreen/RNSplashScreen.m
+++ ios/RNSplashScreen/RNSplashScreen/RNSplashScreen.m
@@ -16,78 +16,103 @@ static RCTRootView *rootView = nil;
 
 @implementation RNSplashScreen
 
-RCT_EXPORT_MODULE(SplashScreen)
+RCT_EXPORT_MODULE(RNSplashScreen)
 
 + (NSString *)splashImageNameForOrientation {
-    CGRect screenRect = [[UIScreen mainScreen] bounds];
-    CGFloat screenWidth = screenRect.size.width;
-    CGFloat screenHeight = screenRect.size.height;
-    CGSize viewSize = CGSizeMake(screenWidth, screenHeight);
+  CGRect screenRect = [[UIScreen mainScreen] bounds];
+  CGFloat screenWidth = screenRect.size.width;
+  CGFloat screenHeight = screenRect.size.height;
+  CGSize viewSize = CGSizeMake(screenWidth, screenHeight);
 
-    UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
+  UIInterfaceOrientation orientation =
+    [UIApplication sharedApplication].statusBarOrientation;
 
-    NSString* viewOrientation = @"Portrait";
-    if (UIDeviceOrientationIsLandscape(orientation)) {
-        viewSize = CGSizeMake(viewSize.height, viewSize.width);
-        viewOrientation = @"Landscape";
-    }
+  NSString* viewOrientation = @"Portrait";
+  if (UIDeviceOrientationIsLandscape(orientation)) {
+    viewSize = CGSizeMake(viewSize.height, viewSize.width);
+    viewOrientation = @"Landscape";
+  }
 
-    NSArray* imagesDict = [[[NSBundle mainBundle] infoDictionary] valueForKey:@"UILaunchImages"];
+  NSArray* imagesDict = [
+    [[NSBundle mainBundle] infoDictionary] valueForKey:@"UILaunchImages"
+  ];
 
-    for (NSDictionary* dict in imagesDict) {
-        CGSize imageSize = CGSizeFromString(dict[@"UILaunchImageSize"]);
-        if (CGSizeEqualToSize(imageSize, viewSize) && [viewOrientation isEqualToString:dict[@"UILaunchImageOrientation"]])
-            return dict[@"UILaunchImageName"];
-    }
-    return nil;
+  for (NSDictionary* dict in imagesDict) {
+    CGSize imageSize = CGSizeFromString(dict[@"UILaunchImageSize"]);
+    if (
+      CGSizeEqualToSize(imageSize, viewSize) &&
+      [viewOrientation isEqualToString:dict[@"UILaunchImageOrientation"]]
+    )
+      return dict[@"UILaunchImageName"];
+  }
+  return nil;
 }
 
 + (void)open:(RCTRootView *)v {
-   rootView = v;
-   UIImageView *view = [[UIImageView alloc]initWithFrame:[UIScreen mainScreen].bounds];
+  rootView = v;
+  UIImageView *view = [
+   [UIImageView alloc]initWithFrame:[UIScreen mainScreen].bounds
+  ];
 
 
-   NSString* launchImage = [RNSplashScreen splashImageNameForOrientation];
+  NSString* launchImage = [RNSplashScreen splashImageNameForOrientation];
 
-   view.image = [UIImage imageNamed:launchImage];
+  view.image = [UIImage imageNamed:launchImage];
 
 
-   [[NSNotificationCenter defaultCenter] removeObserver:rootView name:RCTContentDidAppearNotification object:rootView];
+  [
+    [NSNotificationCenter defaultCenter]
+    removeObserver:rootView
+    name:RCTContentDidAppearNotification
+    object:rootView
+  ];
 
-   [rootView setLoadingView:view];
+  [rootView setLoadingView:view];
 }
 
-RCT_EXPORT_METHOD(close:(NSString *)animationType
-                  duration:(NSInteger)duration
-                  delay:(NSInteger)delay) {
-   if (!rootView) {
-       return;
-   }
+RCT_EXPORT_METHOD(
+  close:(NSString *)animationType
+  duration:(NSInteger)duration
+  delay:(NSInteger)delay
+) {
+  if (!rootView) {
+     return;
+  }
 
-   if([animationType isEqualToString:@"none"]) {
-       rootView.loadingViewFadeDelay = 0;
-       rootView.loadingViewFadeDuration = 0;
-   }
-   else {
-       rootView.loadingViewFadeDelay = delay / 1000.0;
-       rootView.loadingViewFadeDuration = duration / 1000.0;
-   }
-   dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(rootView.loadingViewFadeDelay * NSEC_PER_SEC)),
-                  dispatch_get_main_queue(),
-                  ^{
-                      [UIView animateWithDuration:rootView.loadingViewFadeDuration
-                                       animations:^{
-                                           if([animationType isEqualToString:@"scale"]) {
-                                               rootView.loadingView.transform = CGAffineTransformMakeScale(1.5, 1.5);
-                                               rootView.loadingView.alpha = 0;
-                                           }
-                                           else {
-                                               rootView.loadingView.alpha = 0;
-                                           }
-                                       } completion:^(__unused BOOL finished) {
-                                           [rootView.loadingView removeFromSuperview];
-                                       }];
-                  });
+  if([animationType isEqualToString:@"none"]) {
+    rootView.loadingViewFadeDelay = 0;
+    rootView.loadingViewFadeDuration = 0;
+  } else {
+    rootView.loadingViewFadeDelay = delay / 1000.0;
+    rootView.loadingViewFadeDuration = duration / 1000.0;
+  }
+
+   dispatch_after(
+      dispatch_time(
+       DISPATCH_TIME_NOW,
+       (int64_t)(rootView.loadingViewFadeDelay * NSEC_PER_SEC)
+      ),
+      dispatch_get_main_queue(),
+      ^{
+        [
+          UIView animateWithDuration:rootView.loadingViewFadeDuration
+          animations: ^{
+            if([animationType isEqualToString:@"scale"]) {
+              rootView.loadingView.transform = CGAffineTransformMakeScale(
+                1.5,
+                1.5
+              );
+              rootView.loadingView.alpha = 0;
+            } else {
+              rootView.loadingView.alpha = 0;
+            }
+          }
+          completion: ^(__unused BOOL finished) {
+            [rootView.loadingView removeFromSuperview];
+          }
+        ];
+      }
+  );
 
 }
 
